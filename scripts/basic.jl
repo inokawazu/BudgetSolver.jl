008817133befbb97f8eb0d3d2c69a22028be28e0
Markus-Amano <markus.a.amano@gmail.com>
2023-05-14 16:41:30 +0800

    added example of solver

---
+++ scripts/basic.jl
@@ -0,0 +1,18 @@
+using BudgetSolver
+
+# Parameters
+nmonths = 12*40 # 40 years
+starting_budget = 100.0
+lifestyle_interest = 0.01/12
+overall_interest = 0.02/12
+
+income = [starting_budget for i in 1:nmonths]
+fixed_costs = income ./ 3
+
+ideal_costs = [90.0 * (1 + lifestyle_interest) ^ i for i in 1:nmonths]
+
+scenario = BudgetSolver.FixedICC(starting_budget, income, fixed_costs)
+
+# println(scenario)
+
+solution = BudgetSolver.ipopt_solve(scenario, ideal_costs, overall_interest)
